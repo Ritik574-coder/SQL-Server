06659adde312b6c11044495f73f4032fdde45b38
Ritik <ritik74820@gmail.com>
2026-01-29 10:52:11 +0530

    Create ACID properties.sql

---
+++ SQL Script/SQL Fundamentals/Database Concepts/ACID properties.sql
@@ -0,0 +1,26 @@
+/*
+--==================================================================================================================================
+# ACID Properties
+--==================================================================================================================================
+ACID defines rules that ensure reliable and safe database transactions.
+
+### Atomicity
+A transaction is all or nothing — either fully completed or fully rolled back.
+
+### Consistency
+A transaction moves the database from one valid state to another, following all rules and constraints.
+
+### Isolation
+Multiple transactions run independently without interfering with each other.
+
+### Durability
+Once a transaction is committed, it will not be lost, even after a crash.
+--==================================================================================================================================
+###  One-line Memory Trick
+
+Atomicity = All or nothing
+Consistency = Rules followed
+Isolation = No interference
+Durability = Permanent
+--==================================================================================================================================
+*/
