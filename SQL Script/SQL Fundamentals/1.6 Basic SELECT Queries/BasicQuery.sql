982513584b39ce1fdfc88f683cf5688d1ed0e0eb
Ritik <ritik74820@gmail.com>
2026-02-01 16:33:37 +0530

    Create BasicQuery.sql

---
+++ SQL Script/SQL Fundamentals/1.6 Basic SELECT Queries/BasicQuery.sql
@@ -0,0 +1,14 @@
+-- Select Clause 
+SELECT 
+    first_name ,
+    last_name ,
+    country , 
+    city , 
+    email
+FROM Bronze.crm_employee_info  ;
+
+-- (*) it is use to select all column
+SELECT * FROM Bronze.crm_employee_info  ;
+
+
+
