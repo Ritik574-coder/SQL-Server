a28d52f7369a12564c5d457f2fcbe14087a777b3
Ritik <ritik74820@gmail.com>
2026-02-08 20:47:06 +0530

    Add project description to Employee Directory.sql
    Added project description and schema design for employee management database.

--- SQL Script/SQL Fundamentals/project1.3/Employee Directory.sql
+++ SQL Script/SQL Fundamentals/project1.3/Employee Directory.sql
@@ -1,4 +1,4 @@
-
+/*=============================================================================================
 Project 1.3: Employee Directory
 Description: Create an employee management database
 
@@ -6,3 +6,6 @@ Schema Design:
 
 employees (employee_id, first_name, last_name, email, hire_date, salary, department)
 Write basic queries to explore the data
+
+Author : Ritik__
+=============================================================================================*/
