61300acfa1fd8f97314ccf8136ef242a1097b301
Ritik <ritik74820@gmail.com>
2026-02-14 23:05:49 +0530

    Create 6 Database schema.sql
    /*
    --=============================================================================================================
    Database Schema
    --=============================================================================================================
    A database schema is a logical structure or blueprint that defines how data is organized inside a database.
    
    It specifies:
    
    Tables
    Columns and data types
    Relationships (keys)
    Constraints
    --=============================================================================================================
      
     Short Analogy
    
    Database = Building
    Schema = Floor plan
    --=============================================================================================================
    */

---
+++ SQL_learning_script/SQL Fundamentals/1.1 Database Concepts/6 Database schema.sql
@@ -0,0 +1,20 @@
+/*
+--=============================================================================================================
+Database Schema
+--=============================================================================================================
+A database schema is a logical structure or blueprint that defines how data is organized inside a database.
+
+It specifies:
+
+Tables
+Columns and data types
+Relationships (keys)
+Constraints
+--=============================================================================================================
+  
+ Short Analogy
+
+Database = Building
+Schema = Floor plan
+--=============================================================================================================
+*/
